f4d318a83faf8a6df4bdb4813784d07b8d3c0d95
Robert Schultz <robert@cosmicrealms.com>
2017-03-31 23:43:23 -0400

    wip

--- arduino/MAX6969_7Seg/MAX6969_7Seg.cpp
+++ arduino/MAX6969_7Seg/MAX6969_7Seg.cpp
@@ -5,36 +5,25 @@ MAX6969_7Seg::MAX6969_7Seg()
 {
 }
 
-void MAX6969_7Seg::setup(uint8_t oePin, uint8_t diPin, uint8_t clkPin)
+void MAX6969_7Seg::setup(uint8_t diPin, uint8_t clkPin)
 {
-	this->oePin = oePin;
 	this->diPin = diPin;
 	this->clkPin = clkPin;
 
-	pinMode(oePin, OUTPUT);
 	pinMode(diPin, OUTPUT);
 	pinMode(clkPin, OUTPUT);
 
-	digitalWrite(oePin, HIGH);
 	digitalWrite(diPin, LOW);
 	digitalWrite(clkPin, LOW);
 
-	flip = (oePin==40 ? true : false);	// TEMPORARY
-}
+	clear();
 
-void MAX6969_7Seg::on(void)
-{
-	digitalWrite(oePin, LOW);
-}
-
-void MAX6969_7Seg::off(void)
-{
-	digitalWrite(oePin, HIGH);
+	flip = (diPin>40 ? true : false);	// TEMPORARY
 }
 
 void MAX6969_7Seg::clear(void)
 {
-	shift16(0);
+	shift16(0x0000);
 }
 
 void MAX6969_7Seg::print(char c)
--- arduino/MAX6969_7Seg/MAX6969_7Seg.h
+++ arduino/MAX6969_7Seg/MAX6969_7Seg.h
@@ -7,16 +7,13 @@ class MAX6969_7Seg
 {
 	public:
 		MAX6969_7Seg();
-		void setup(uint8_t oePin, uint8_t diPin, uint8_t clkPin);
+		void setup(uint8_t diPin, uint8_t clkPin);
 		void clear(void);
-		void on(void);
-		void off(void);
 		void print(char c);
 
 	private:
 		void shift16(uint16_t data);
 
-		uint8_t oePin;
 		uint8_t diPin;
 		uint8_t clkPin;
 		uint8_t flip;	// TEMPORARY
