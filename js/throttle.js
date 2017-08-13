2a204ccd8d06ad455778bfb656306caeabbf8181
Robert Schultz <robert@cosmicrealms.com>
2017-08-13 10:00:13 -0400

    Added support for always sending the last event, waiting until the min time has passed if necessary

--- js/throttle.js
+++ js/throttle.js
@@ -2,19 +2,38 @@
 
 (function(exports)
 {
-	function throttle(f, minInterval)
+	function throttle(f, minInterval, reSendLastEvent)
 	{
 		var lastCalled = 0;
+		var lastEventTimeout = null;
 
 		var throttler = function()
 		{
-			if((Date.now()-lastCalled)<minInterval)
+			var timePassed = (Date.now()-lastCalled);
+			if(timePassed<minInterval)
+			{
+				if(reSendLastEvent)
+					scheduleNextThrottle(Array.prototype.slice.call(arguments), (minInterval-timePassed)+1);
 				return;
+			}
+
+			if(lastEventTimeout)
+			{
+				clearTimeout(lastEventTimeout);
+				lastEventTimeout = null;
+			}
 
 			lastCalled = Date.now();
 
 			return f.apply(f, Array.toArray(arguments));
 		};
+
+		function scheduleNextThrottle(lastArgs, waitDuration)
+		{
+			if(lastEventTimeout)
+				clearTimeout(lastEventTimeout);
+			lastEventTimeout = setTimeout(function() { lastEventTimeout = null; throttler.apply(throttler, lastArgs); }, waitDuration);
+		}
 
 		return throttler;
 	}
