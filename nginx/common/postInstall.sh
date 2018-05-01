d92cb09ae13b6b4d6fecc836f0e653baa0d1914f
Robert Schultz <robert@cosmicrealms.com>
2018-05-01 15:58:05 -0400

    Updated nginx files to use my own mime.types file

--- nginx/common/postInstall.sh
+++ nginx/common/postInstall.sh
@@ -9,5 +9,5 @@ rm -f /usr/local/nginx/conf/win-utf /usr/local/nginx/conf/koi*
 # Remove the default nginx conf file
 rm -f /usr/local/nginx/conf/nginx.conf.default
 
-# Don't need the default mime types file, we have our own mime file from https://github.com/h5bp/server-configs-nginx/
-rm -f /usr/local/nginx/conf/mime.types.default
+# Don't need the default mime types file, we have our own mime file
+rm -f /usr/local/nginx/conf/mime.types*
--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -2,4 +2,3 @@
 
 scp -r /mnt/compendium/DevLab/common/nginx/common/ $1:/usr/local/nginx/conf/
 scp /mnt/compendium/DevLab/common/nginx/nginx.conf $1:/usr/local/nginx/conf/
-scp /mnt/compendium/DevLab/common/lib/nginx/server-configs-nginx/mime.types $1:/usr/local/nginx/conf/
